17eaac671b637e5e408cb636a52b4e9b219ec77b
DEEMANO4 <apenadavid18@gmail.com>
2025-10-17 06:54:07 +0100

    DB

---
+++ alx_book_store.sql
@@ -0,0 +1,36 @@
+DataBase = alx_book_store
+
+
+
+CREATE Book (
+    book_id INT Primary Key,
+    title VARCHAR(130) NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+    price DOUBLE(10,2)
+    publication_date DATE
+);
+
+CREATE Author (
+    author_id INT Primary Key,
+    author_name VARCHAR(215) NOT NULL,
+);
+
+CREATE Customers (
+    customer_id INT Primary Key,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE,
+    adress TEXT
+);
+
+CREATE Orders (
+    order_id INT Primary Key,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+    order_date Date
+);
+
+CREATE Orders_Details (
+    orderdetailid INT Primary Key,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+    FOREIGN KEY(book_id) REFERENCES Book(book_id)
+    quantity DOUBLE(10,2)
+)
