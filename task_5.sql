029f72ddf6e13b53b1fef4f599a16c5749c6d683
DEEMANO4 <apenadavid18@gmail.com>
2025-10-17 10:12:05 +0100

    DB

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
-sql = "INSERT INTO Customers (customer_id, customer_name, email, address) VALUES (%s, %s)"
+sql = "INSERT INTO customer (customer_id, customer_name, email, address) VALUES (%s, %s)"
 val = (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.")
